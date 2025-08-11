c448de9171e6ac2540a686effc9457416ec0035a
ronbunno <ronbunno7@gmail.comv>
2025-08-11 11:02:42 +0700

    fix 404

--- src/app/guards/game-id-guard.ts
+++ src/app/guards/game-id-guard.ts
@@ -5,8 +5,6 @@ import { gamesData } from '../data/games.data';
 export const gameIdGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const id = Number(route.paramMap.get('id'));
-
-  // Check if the ID exists in the games list
   const exists = gamesData.some(game => game.id === id);
 
   if (!exists) {
